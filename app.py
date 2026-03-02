8bb7a1cc1a5d374abb0f2cff151955b3390306e5
ps596389-source <ps596389@gmail.com>
2026-03-03 03:36:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,17 +23,18 @@ st.markdown("""
 st.title("Hi [suruchi]! ❤️")
 st.header("Mujhe tumse kuch kehna hai...")
 
-st.write("Hum jab se mile hain, meri life badal gayi hai. Kya tum meri life ka permanent hissa banogi?")
+st.write("Hum jab se mile hain, meri life badal gayi hai. Kya tum mujhse shaadi krogi👀?")
 
 col1, col2 = st.columns(2)
 
 with col1:
     if st.button("YES! 😍"):
         st.balloons()
-        st.success("I love you too! ❤️ Main duniya ka sabse khush-kismat insaan hoon!")
+        st.success("I love you too meri jaan! ❤️ Main duniya ka sabse khush-kismat insaan hoon!")
         st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHIyb3RrZzR4ZzR4ZzR4ZzR4ZzR4ZzR4ZzR4ZzR4ZzR4ZzR4ZzR4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCBmcm9tX2dpZl9zZWFyY2hfcmVzdWx0cyZjdD1n/KztT2c4u8mYYUiCi7W/giphy.gif")
 
 with col2:
     no_clicked = st.button("No 😢")
     if no_clicked:
-        st.warning("Oops! Try again... Ye button kaam nahi kar raha! 😉")
+        st.warning("beta aisa koi option hi na h na ka is pr click kra toh thappad khaogi")
+st.slape()
